e774bba1207c2eb21822cebbd5ddec14b5cd91ff
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-14 08:08:37 +0200

    Add changes regarding review

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -10,13 +10,13 @@ public class Main {
         while (!isGoodNumber) {
             System.out.println("Podaj liczbę");
             int number = scanner.nextInt();
-            if(number < 100)
+            if (number < 100) {
                 System.out.println("Podana liczba jest za mała");
-            else if (number > 200)
+            } else if (number > 200) {
                 System.out.println("Podana liczba jest za duża");
-            else if(number % 3 != 0)
+            } else if (number % 3 != 0) {
                 System.out.println("Liczba nie jest podzielna przez 3");
-            else {
+            } else {
                 System.out.println("Twoja liczba jest ok");
                 isGoodNumber = true;
             }
